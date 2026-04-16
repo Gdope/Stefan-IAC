e60c884100a2ae0c7be0cb4bc46bcf68c07f3a16
Gdope <snedeljkovic2003@gmail.com>
2026-04-16 22:46:10 +0200

    vpc.tf

--- vars.tf
+++ vars.tf
@@ -9,3 +9,45 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+variable "VpcCIDR" {
+  default = "172.21.0.0/16"
+}
+variable "PubSub1CIDR" {
+  default = "172.21.1.0/24"
+}
+
+variable "PubSub2CIDR" {
+  default = "172.21.2.0/24"
+}
+
+variable "PubSub3CIDR" {
+  default = "172.21.3.0/24"
+}
+
+variable "PrivSub1CIDR" {
+  default = "172.21.4.0/24"
+}
+
+variable "PrivSub2CIDR" {
+  default = "172.21.5.0/24"
+}
+
+variable "PrivSub3CIDR" {
+  default = "172.21.6.0/24"
+}
+
+variable "VPC_NAME" {
+  default = "vprofile-VPC"
+}
+
+variable "Zone1" {
+  default = "us-east-1a"
+}
+
+variable "Zone2" {
+  default = "us-east-1b"
+}
+
+variable "Zone3" {
+  default = "us-east-1c"
+}
