cd9a39fdc11ca0a8d125a2726b6178a93b177412
Gdope <snedeljkovic2003@gmail.com>
2026-04-17 01:53:20 +0200

    backend_instance added

--- backend_instance.tf
+++ backend_instance.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "stefan_mc01" {
   subnet_id              = module.vpc.private_subnets[0]
   count                  = var.instance_count
   vpc_security_group_ids = [aws_security_group.backend_sg.id]
-  user_data              = file("${path.module}/user_data/memcached.sh")
+  user_data              = file("${path.module}/scripts/user_data/memcached.sh")
 
   tags = {
     Name = "stefan-memcached"
@@ -50,25 +50,10 @@ resource "aws_instance" "stefan_rmq01" {
   subnet_id              = module.vpc.private_subnets[0]
   count                  = var.instance_count
   vpc_security_group_ids = [aws_security_group.backend_sg.id]
-  user_data              = file("${path.module}/user_data/rabbitmq.sh")
+  user_data              = file("${path.module}/scripts/user_data/rabbitmq.sh")
 
   tags = {
     Name = "stefan-rabbitmq"
-
-  }
-
-}
-resource "aws_instance" "stefan_app01" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.micro"
-  key_name               = aws_key_pair.iac_key_tf.key_name
-  subnet_id              = module.vpc.private_subnets[0]
-  count                  = var.instance_count
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
-  user_data              = file("${path.module}/user_data/app.sh")
-
-  tags = {
-    Name = "stefan-app"
 
   }
 
