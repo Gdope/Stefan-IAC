d76c5e8269d035720309bd67be3fad6449625994
Gdope <snedeljkovic2003@gmail.com>
2026-04-17 01:51:54 +0200

    Renamed files and fixed path to user_data

---
+++ app_instance.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "stefan_app01" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.iac_key_tf.key_name
+  subnet_id              = module.vpc.private_subnets[0]
+  count                  = var.instance_count
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  user_data              = file("${path.module}/scripts/user_data/app.sh")
+
+  tags = {
+    Name = "stefan-app"
+
+  }
+
+}
