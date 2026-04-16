581c9afbe251fd604ec393fe60b91ec49077bb0a
Gdope <snedeljkovic2003@gmail.com>
2026-04-17 00:52:53 +0200

    Fixed key_name

--- bastion-host.tf
+++ bastion-host.tf
@@ -17,7 +17,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "stefan_bastion_host" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
-  key_name               = aws_key_pair.iac_key_tf_name
+  key_name               = aws_key_pair.iac_key_tf.key_name
   subnet_id              = module.vpc.public_subnets[0]
   count                  = var.instance_count
   vpc_security_group_ids = [aws_security_group.bastion_sg]
