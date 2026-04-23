be788bb1a34834d159c10ccd6f89a4291dc29b9f
Gdope <snedeljkovic2003@gmail.com>
2026-04-23 12:16:04 +0200

    Updated bastion-sg

--- secgrp.tf
+++ secgrp.tf
@@ -50,6 +50,13 @@ resource "aws_vpc_security_group_ingress_rule" "ssh_my_ip" {
   ip_protocol       = "tcp"
   to_port           = 22
 }
+resource "aws_vpc_security_group_ingress_rule" "ssh_from_anywhere_to_bastion" {
+  security_group_id = aws_security_group.bastion_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
 
 resource "aws_vpc_security_group_egress_rule" "allow_AllOutbound_ipv4_bastion" {
   security_group_id = aws_security_group.bastion_sg.id
